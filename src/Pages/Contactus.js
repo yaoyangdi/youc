62063b37bbcc9439ba445021ed75fa4d7804df57
steven ding <dingyaoyang2019@gmail.com>
2021-12-28 23:20:21 +1100

    contactus page button added.

--- src/Pages/Contactus.js
+++ src/Pages/Contactus.js
@@ -16,26 +16,30 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 import './Contactus.css';
 
+
 const theme = createTheme({
-    status: {
-      danger: '#e53e3e',
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
     },
-    palette: {
-      primary: {
-        main: '#0971f1',
-        darker: '#053e85',
-      },
-      neutral: {
-        main: '#f07c28',
-        contrastText: '#fff',
-      },
-  
-      secondary: {
-        // This is green.A700 as hex.
-        main: '#f4caab',
-      },
+    neutral: {
+      main: 'rgb(236, 178, 16)',
+      contrastText: '#fff',
     },
-  });
+
+    secondary: {
+      main: 'rgb(223, 163, 32)',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    fontSize: '20',
+    }
+});
 const mapContainerStyle={
 	width:"100%",
 	height:"450px",
@@ -99,18 +103,19 @@ export default function Contactus() {
                           <form action="" method="get" >
                             <p>Full Name</p>
 
-                            <p><input id="GET-name" type="text"  name="name" style={{width:'90%',fontSize:'25px',width:'300px',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                            <p><input id="GET-name" type="text"  name="name" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
                             <p>Phone</p>
-                            <p><input id="GET-name" type="text"  name="phone" style={{width:'90%',fontSize:'25px',width:'300px',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                            <p><input id="GET-name" type="text"  name="phone" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
                             <p>Email</p>
-                            <p><input id="GET-name" type="text"  name="email" style={{width:'90%',fontSize:'25px',width:'300px',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                            <p><input id="GET-name" type="text"  name="email" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
                             <p>Message</p>  
 
-                            <textarea name="message" rows="5" cols="50" placeholder="Message" style={{width:'90%', fontSize:'25px',width:"300px",border:'1px solid #BBB3B9'}}></textarea>
+                            <textarea name="message" rows="5" cols="50" placeholder="Message" style={{width:'90%', fontSize:'25px',width:"90%",border:'1px solid #BBB3B9'}}></textarea>
                           </form>
+                          <Button color="secondary" variant="contained" size='large'>Submit</Button>
                           </ThemeProvider>
                       </Box>
                 </div>
