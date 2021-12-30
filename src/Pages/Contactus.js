a6d4fe41bae0259d50a7645f4e990b18694ed9aa
steven ding <dingyaoyang2019@gmail.com>
2021-12-30 20:48:37 +1100

    UI improved 1) sidebar faded effect added. 2) sidebar responsive font size modified. 3) contactus page UI modified.

--- src/Pages/Contactus.js
+++ src/Pages/Contactus.js
@@ -3,8 +3,6 @@ import Box from '@mui/material/Box';
 import Button from '../components/Button';
 import Footer from '../components/Footer/Footer';
 
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/material/styles';
 import {
 	GoogleMap,
 	LoadScript,
@@ -14,30 +12,6 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 import './Contactus.css';
 
-
-const theme = createTheme({
-  status: {
-    danger: '#e53e3e',
-  },
-  palette: {
-    primary: {
-      main: '#0971f1',
-      darker: '#053e85',
-    },
-    neutral: {
-      main: 'rgb(236, 178, 16)',
-      contrastText: '#fff',
-    },
-
-    secondary: {
-      main: 'rgb(223, 163, 32)',
-      contrastText: '#fff',
-    },
-  },
-  typography: {
-    fontSize: '20',
-    }
-});
 const mapContainerStyle={
 	width:"100%",
 	height:"450px",
@@ -58,66 +32,62 @@ export default function Contactus() {
 
             <div className="main">
 
-                <div style={{width:'100%'}}>
-                    <LoadScript 
-                        googleMapsApiKey="AIzaSyAoJxtfyCL9v5Xc-wpKUbYbuArrvJViTEc">
-                            <GoogleMap
-                                mapContainerStyle={mapContainerStyle}
-                                center={center}
-                                zoom={10}>
-                                <Marker position={center}/>
-                            </GoogleMap>
-                    </LoadScript>
-                </div>
+                <LoadScript 
+                    googleMapsApiKey="AIzaSyAoJxtfyCL9v5Xc-wpKUbYbuArrvJViTEc">
+                        <GoogleMap
+                            mapContainerStyle={mapContainerStyle}
+                            center={center}
+                            zoom={10}>
+                            <Marker position={center}/>
+                        </GoogleMap>
+                </LoadScript>
 
                 <div className='textItemsss'>
                       <Box className='textItem1'>
-                          <ThemeProvider theme={theme}>
-                              <h1>Our Showroom</h1>
-                                <p color='#f4caab'> Book a FREE consultation and discover how we can help you.</p>
-                              <div className='shownroom-text'>
-                                <p><FiberManualRecordIcon style={{width:'45px',height:'25px',color:"#f4caab"}}/>Design to suit your situation</p>
-                                <p><FiberManualRecordIcon style={{width:'45px',height:'25px',color:"#f4caab"}}/>Full range of options across different manufacturers</p>
-                                <p><FiberManualRecordIcon style={{width:'45px',height:'25px',color:"#f4caab"}}/>Extensive range to meet individual budgets</p>
-                              </div>
-                              
-                              <h2 style={{color:"#f4caab"}}>Contact us</h2>
-                              <div className='contact-text'>
-                                <p>03 9543 2228</p>
-                                <p>info@youc.com.au</p>
-                                <p>Monday-Friday:  9am-2pm</p>
-                              </div>
-                              <br></br>
-                              <h2 style={{color:"#f4caab"}}>Address</h2>
-                              <div className='contact-text'>
-                                <p>43 Nicholaas Drive</p>
-                                <p>monday-friday 9am-2pm</p>
-                              </div>
+                          <h1 className='title'>Our Showroom</h1>
+                          <div className='--p'>
+                            <p color='#f4caab'> Book a FREE consultation and discover how we can help you.</p>
+                          </div>
+                          <div className='shownroom-text'>
+                            <p><FiberManualRecordIcon style={{width:'25px',height:'14px',color:"rgb(213, 171, 132)"}}/>Design to suit your situation</p>
+                            <p><FiberManualRecordIcon style={{width:'25px',height:'14px',color:"rgb(213, 171, 132)"}}/>Full range of options across different manufacturers</p>
+                            <p><FiberManualRecordIcon style={{width:'25px',height:'14px',color:"rgb(213, 171, 132)"}}/>Extensive range to meet individual budgets</p>
+                            <br></br>
+                          </div>
+                          
+                          <h2 style={{color:"rgb(213, 171, 132)"}}>Contact us</h2>
+                          <div className='contact-text'>
+                            <p>03 9543 2228</p>
+                            <p>info@youc.com.au</p>
+                            <p>Monday-Friday:  9am-2pm</p>
+                          </div>
+                          <h2 style={{color:"rgb(213, 171, 132)"}}>Address</h2>
+                          <div className='contact-text'>
+                            <p>43 Nicholaas Drive</p>
+                            <p>monday-friday 9am-2pm</p>
+                          </div>
 
-                          </ThemeProvider>
                       </Box>
 
                       <Box className='textItem2'>
-                          <ThemeProvider theme={theme}>
-                          <h1>Contact Us</h1>
-                          <form action="" method="get" >
-                            <p>Full Name</p>
+                        <h1 className='title'>Contact Us</h1>
+                        <form action="" method="get" >
+                          <p>Full Name</p>
 
-                            <p><input id="GET-name" type="text"  name="name" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                          <p><input id="GET-name" type="text"  name="name" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
-                            <p>Phone</p>
-                            <p><input id="GET-name" type="text"  name="phone" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                          <p>Phone</p>
+                          <p><input id="GET-name" type="text"  name="phone" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
-                            <p>Email</p>
-                            <p><input id="GET-name" type="text"  name="email" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
+                          <p>Email</p>
+                          <p><input id="GET-name" type="text"  name="email" style={{width:'90%',fontSize:'25px',width:'90%',height:'40px',border:'1px solid #BBB3B9'}}/></p>
 
-                            <p>Message</p>  
+                          <p>Message</p>  
 
-                            <textarea name="message" rows="5" cols="50" placeholder="Message" style={{width:'90%', fontSize:'25px',width:"90%",border:'1px solid #BBB3B9'}}></textarea>
-                          </form>
-                          <br></br>
-                          <Button href='/#' href="/contact-us" buttonStyle='btn--primary' buttonSize='btn--md' name='Make an Enquiry'/>
-                          </ThemeProvider>
+                          <textarea name="message" rows="5" cols="50" placeholder="Message" style={{width:'90%', fontSize:'25px',width:"90%",border:'1px solid #BBB3B9'}}></textarea>
+                        </form>
+                        <br></br>
+                        <Button href='/#' href="/contact-us" buttonStyle='btn--primary' buttonSize='btn--lg' name='Submit'/>
                       </Box>
                 </div>
                 <Footer />
